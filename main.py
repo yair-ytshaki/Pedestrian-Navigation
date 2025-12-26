bf15a3756718b472825a103b7c2368affcd682d1
Yair Ytshaki <145800935+yair-ytshaki@users.noreply.github.com>
2025-12-26 16:13:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Object Oriented Programming for Geographical Information- 14845
+# Spring 2023
+# H.W. 5
+# Saar Kariv 316557743 & Yair Ytshaki 208250597
+
+from UI import UI
+
+if __name__ == "__main__":
+    app = UI()
+    app.run()
