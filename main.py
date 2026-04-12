7a1c20f25c0370f70deb80416d4af86c5e588808
Yair Ytshaki <145800935+yair-ytshaki@users.noreply.github.com>
2026-04-12 20:48:32 +0300

    Remove commented header from main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Object Oriented Programming for Geographical Information- 14845
-# Spring 2023
-# H.W. 5
-# Saar Kariv 316557743 & Yair Ytshaki 208250597
-
 from UI import UI
 
 if __name__ == "__main__":
